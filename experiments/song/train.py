3ddb326edad15baadb01446ec41f1903d55797df
FacerAin <ywsong.dev@kakao.com>
2022-10-25 14:26:04 +0000

    add text_cleaning

--- experiments/song/train.py
+++ experiments/song/train.py
@@ -1,4 +1,7 @@
 import argparse
+import os
+import re
+import emoji
 
 import pandas as pd
 
@@ -11,6 +14,16 @@ import pytorch_lightning as pl
 
 from pytorch_lightning.loggers import WandbLogger
 from pytorch_lightning.callbacks import ModelCheckpoint
+
+from soynlp.normalizer import repeat_normalize
+
+import wandb
+
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+pattern = re.compile(f"[^ .,?!/@$%~％·∼()\x00-\x7Fㄱ-ㅣ가-힣]+")
+url_pattern = re.compile(
+    r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)"
+)
 
 
 class Dataset(torch.utils.data.Dataset):
@@ -32,7 +45,17 @@ class Dataset(torch.utils.data.Dataset):
 
 
 class Dataloader(pl.LightningDataModule):
-    def __init__(self, model_name, batch_size, shuffle, train_path, dev_path, test_path, predict_path, num_workers):
+    def __init__(
+        self,
+        model_name,
+        batch_size,
+        shuffle,
+        train_path,
+        dev_path,
+        test_path,
+        predict_path,
+        num_workers,
+    ):
         super().__init__()
         self.model_name = model_name
         self.batch_size = batch_size
@@ -49,23 +72,45 @@ class Dataloader(pl.LightningDataModule):
         self.test_dataset = None
         self.predict_dataset = None
 
-        self.tokenizer = transformers.AutoTokenizer.from_pretrained(model_name, max_length=160)
-        self.target_columns = ['label']
-        self.delete_columns = ['id']
-        self.text_columns = ['sentence_1', 'sentence_2']
+        self.tokenizer = transformers.AutoTokenizer.from_pretrained(
+            model_name, max_length=160
+        )
+        # self.tokenizer.add_tokens(["<PERSON>"])
+        self.target_columns = ["label"]
+        self.delete_columns = ["id"]
+        self.text_columns = ["sentence_1", "sentence_2"]
+
+    def text_cleaning(self, text):
+        # Ref: https://github.com/Beomi/KcBERT
+        text = pattern.sub(" ", text)
+        text = emoji.replace_emoji(text, replace="")
+        text = url_pattern.sub("", text)
+        text = text.strip()
+        text = repeat_normalize(text, num_repeats=2)
+        return text
 
     def tokenizing(self, dataframe):
         data = []
-        for idx, item in tqdm(dataframe.iterrows(), desc='tokenizing', total=len(dataframe)):
+        for idx, item in tqdm(
+            dataframe.iterrows(), desc="tokenizing", total=len(dataframe)
+        ):
             # 두 입력 문장을 [SEP] 토큰으로 이어붙여서 전처리합니다.
-            text = '[SEP]'.join([item[text_column] for text_column in self.text_columns])
-            outputs = self.tokenizer(text, add_special_tokens=True, padding='max_length', truncation=True)
-            data.append(outputs['input_ids'])
+            text = "[SEP]".join(
+                [item[text_column] for text_column in self.text_columns]
+            )
+            outputs = self.tokenizer(
+                text, add_special_tokens=True, padding="max_length", truncation=True
+            )
+            data.append(outputs["input_ids"])
         return data
 
     def preprocessing(self, data):
         # 안쓰는 컬럼을 삭제합니다.
         data = data.drop(columns=self.delete_columns)
+
+        # Text Cleaning
+        for text_column in self.text_columns:
+            data[text_column] = data[text_column].apply(self.text_cleaning)
 
         # 타겟 데이터가 없으면 빈 배열을 리턴합니다.
         try:
@@ -77,8 +122,8 @@ class Dataloader(pl.LightningDataModule):
 
         return inputs, targets
 
-    def setup(self, stage='fit'):
-        if stage == 'fit':
+    def setup(self, stage="fit"):
+        if stage == "fit":
             # 학습 데이터와 검증 데이터셋을 호출합니다
             train_data = pd.read_csv(self.train_path)
             val_data = pd.read_csv(self.dev_path)
@@ -103,16 +148,29 @@ class Dataloader(pl.LightningDataModule):
             self.predict_dataset = Dataset(predict_inputs, [])
 
     def train_dataloader(self):
-        return torch.utils.data.DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=args.shuffle, num_workers=self.num_workers)
+        return torch.utils.data.DataLoader(
+            self.train_dataset,
+            batch_size=self.batch_size,
+            shuffle=args.shuffle,
+            num_workers=self.num_workers,
+        )
 
     def val_dataloader(self):
-        return torch.utils.data.DataLoader(self.val_dataset, batch_size=self.batch_size, num_workers=self.num_workers)
+        return torch.utils.data.DataLoader(
+            self.val_dataset, batch_size=self.batch_size, num_workers=self.num_workers
+        )
 
     def test_dataloader(self):
-        return torch.utils.data.DataLoader(self.test_dataset, batch_size=self.batch_size, num_workers=self.num_workers)
+        return torch.utils.data.DataLoader(
+            self.test_dataset, batch_size=self.batch_size, num_workers=self.num_workers
+        )
 
     def predict_dataloader(self):
-        return torch.utils.data.DataLoader(self.predict_dataset, batch_size=self.batch_size, num_workers=self.num_workers)
+        return torch.utils.data.DataLoader(
+            self.predict_dataset,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+        )
 
 
 class Model(pl.LightningModule):
@@ -125,12 +183,13 @@ class Model(pl.LightningModule):
 
         # 사용할 모델을 호출합니다.
         self.plm = transformers.AutoModelForSequenceClassification.from_pretrained(
-            pretrained_model_name_or_path=model_name, num_labels=1)
+            pretrained_model_name_or_path=model_name, num_labels=1
+        )
         # Loss 계산을 위해 사용될 L1Loss를 호출합니다.
         self.loss_func = torch.nn.L1Loss()
 
     def forward(self, x):
-        x = self.plm(x)['logits']
+        x = self.plm(x)["logits"]
 
         return x
 
@@ -147,8 +206,10 @@ class Model(pl.LightningModule):
         logits = self(x)
         loss = self.loss_func(logits, y.float())
         self.log("val_loss", loss)
-
-        self.log("val_pearson", torchmetrics.functional.pearson_corrcoef(logits.squeeze(), y.squeeze()))
+        self.log(
+            "val_pearson",
+            torchmetrics.functional.pearson_corrcoef(logits.squeeze(), y.squeeze()),
+        )
 
         return loss
 
@@ -156,7 +217,10 @@ class Model(pl.LightningModule):
         x, y = batch
         logits = self(x)
 
-        self.log("test_pearson", torchmetrics.functional.pearson_corrcoef(logits.squeeze(), y.squeeze()))
+        self.log(
+            "test_pearson",
+            torchmetrics.functional.pearson_corrcoef(logits.squeeze(), y.squeeze()),
+        )
 
     def predict_step(self, batch, batch_idx):
         x = batch
@@ -169,41 +233,58 @@ class Model(pl.LightningModule):
         return optimizer
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # 하이퍼 파라미터 등 각종 설정값을 입력받습니다
     # 터미널 실행 예시 : python3 run.py --batch_size=64 ...
     # 실행 시 '--batch_size=64' 같은 인자를 입력하지 않으면 default 값이 기본으로 실행됩니다
     torch.cuda.empty_cache()
-    
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--model_name', default='klue/roberta-base', type=str)
-    parser.add_argument('--batch_size', default=32, type=int)
-    parser.add_argument('--max_epoch', default=5, type=int)
-    parser.add_argument('--shuffle', default=True)
-    parser.add_argument('--learning_rate', default=1e-5, type=float)
-    parser.add_argument('--num_workers', default=4, type=int)
-    parser.add_argument('--train_path', default='../../data/train.csv')
-    parser.add_argument('--dev_path', default='../../data/dev.csv')
-    parser.add_argument('--test_path', default='../../data/dev.csv')
-    parser.add_argument('--predict_path', default='../../data/test.csv')
-    args = parser.parse_args(args=[])
 
-    wandb_logger = WandbLogger(project="STS", log_model="all")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_name", default="klue/roberta-small", type=str)
+    parser.add_argument("--batch_size", default=32, type=int)
+    parser.add_argument("--max_epoch", default=10, type=int)
+    parser.add_argument("--shuffle", default=True, type=bool)
+    parser.add_argument("--wandb_offline", default=False, type=bool)
+    parser.add_argument("--learning_rate", default=1e-5, type=float)
+    parser.add_argument("--num_workers", default=4, type=int)
+    parser.add_argument("--train_path", default="../../data/train.csv", type=str)
+    parser.add_argument("--dev_path", default="../../data/dev.csv", type=str)
+    parser.add_argument("--test_path", default="../../data/dev.csv", type=str)
+    parser.add_argument("--predict_path", default="../../data/test.csv", type=str)
+    args = parser.parse_args()
+
+    wandb_name = f"{args.model_name}_lr_{args.learning_rate}"
+
+    wandb_logger = WandbLogger(
+        name=wandb_name, project="STS", offline=args.wandb_offline
+    )
     wandb_logger.experiment.config.update(args)
 
     # dataloader와 model을 생성합니다.
-    dataloader = Dataloader(args.model_name, args.batch_size, args.shuffle, args.train_path, args.dev_path,
-                            args.test_path, args.predict_path, args.num_workers)
+    dataloader = Dataloader(
+        args.model_name,
+        args.batch_size,
+        args.shuffle,
+        args.train_path,
+        args.dev_path,
+        args.test_path,
+        args.predict_path,
+        args.num_workers,
+    )
+
+    print(len(dataloader.train_dataloader()))
     model = Model(args.model_name, args.learning_rate)
 
-    checkpoint_callback = ModelCheckpoint(save_top_k=3, mode = 'max', monitor = "val_pearson")
+    # checkpoint_callback = ModelCheckpoint(save_top_k=3, mode = 'max', monitor = "val_pearson")
 
     # gpu가 없으면 'gpus=0'을, gpu가 여러개면 'gpus=4'처럼 사용하실 gpu의 개수를 입력해주세요
-    trainer = pl.Trainer(logger=wandb_logger, gpus=1, max_epochs=args.max_epoch, log_every_n_steps=1, callbacks=[checkpoint_callback])
+    trainer = pl.Trainer(
+        logger=wandb_logger, gpus=1, max_epochs=args.max_epoch, log_every_n_steps=1
+    )
 
     # Train part
     trainer.fit(model=model, datamodule=dataloader)
     trainer.test(model=model, datamodule=dataloader)
 
     # 학습이 완료된 모델을 저장합니다.
-    torch.save(model, 'model.pt')
+    torch.save(model, "model.pt")
