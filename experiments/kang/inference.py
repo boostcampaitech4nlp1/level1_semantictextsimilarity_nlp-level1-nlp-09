b62bb49721775347cc048fc2bcc0bac0bf665f0b
kyc3492 <kyc3492@gmail.com>
2022-10-27 10:38:07 +0000

    Changed model to -v2022, added RTT-dev

--- experiments/kang/inference.py
+++ experiments/kang/inference.py
@@ -236,43 +236,28 @@ if __name__ == "__main__":
     parser.add_argument("--shuffle", default=True)
     parser.add_argument("--learning_rate", default=2e-5, type=float)
     parser.add_argument("--warmup_ratio", default=0.1, type=float)
-    parser.add_argument("--train_path", default="../../data/train.csv")
-    parser.add_argument("--dev_path", default="../../data/dev.csv")
-    parser.add_argument("--test_path", default="../../data/dev.csv")
-    parser.add_argument("--predict_path", default="../../data/test.csv")
+    parser.add_argument("--train_path", default="../../../data/train.csv")
+    parser.add_argument("--dev_path", default="../../../data/dev.csv")
+    parser.add_argument("--test_path", default="../../../data/dev.csv")
+    parser.add_argument("--predict_path", default="../../../data/test.csv")
     args = parser.parse_args(args=[])
 
     # dataloader와 model을 생성합니다.
-    dataloader = Dataloader(
-        args.model_name,
-        args.batch_size,
-        args.shuffle,
-        args.train_path,
-        args.dev_path,
-        args.test_path,
-        args.predict_path,
-    )
-
-    model = Model(
-        args.model_name, args.learning_rate, args.max_epoch, args.warmup_ratio
-    )
+    dataloader = Dataloader(args.model_name, args.batch_size, args.shuffle, args.train_path, args.dev_path,
+                            args.test_path, args.predict_path)
 
     # gpu가 없으면 'gpus=0'을, gpu가 여러개면 'gpus=4'처럼 사용하실 gpu의 개수를 입력해주세요
     trainer = pl.Trainer(gpus=1, max_epochs=args.max_epoch, log_every_n_steps=1)
 
     # Inference part
     # 저장된 모델로 예측을 진행합니다.
-    checkpoint = torch.load("STS/li88gt8a/checkpoints/epoch=29-step=8760.ckpt")
-    print(checkpoint.keys())
-
-    model.load_state_dict(checkpoint["state_dict"])
-    trainer.test(model=model, datamodule=dataloader)
+    model = torch.load('model.pt')
     predictions = trainer.predict(model=model, datamodule=dataloader)
 
     # 예측된 결과를 형식에 맞게 반올림하여 준비합니다.
     predictions = list(round(float(i), 1) for i in torch.cat(predictions))
 
     # output 형식을 불러와서 예측된 결과로 바꿔주고, output.csv로 출력합니다.
-    output = pd.read_csv("../../data/sample_submission.csv")
-    output["target"] = predictions
-    output.to_csv("output.csv", index=False)
+    output = pd.read_csv('../../../sample_submission.csv')
+    output['target'] = predictions
+    output.to_csv('output.csv', index=False)
